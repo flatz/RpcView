efa3f3b9be584ff09062bdcb732e9233392dd8dd
Alexey Kulaev <alex.qart@gmail.com>
2019-12-07 21:12:24 +0300

    Fix compilation issues

--- RpcDecompiler/InternalsRpcUtils.cpp
+++ RpcDecompiler/InternalsRpcUtils.cpp
@@ -1,5 +1,7 @@
 #include "internalRpcUtils.h"
+#include <codecvt>
 #include <list>
+#include <locale>
 #include <sstream>
 
 
@@ -32,14 +34,8 @@ BOOL __fastcall isStandardCharacter(_In_ const WCHAR wc)
 std::string narrow(
 	_In_	const std::wstring& ws)
 {
-	//std::vector<char> buffer(ws.size());
-	////std::locale loc("english");
-	//std::locale loc;
-	//std::use_facet< std::ctype<wchar_t> > (loc).narrow(ws.data(), ws.data() + ws.size(), '?', &buffer[0]);
-
-	//return std::string(&buffer[0], buffer.size());
-
-	return std::string(ws.begin(), ws.end());
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	return std::string(converter.to_bytes(ws));
 }
 
 
